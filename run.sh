fda39ce240416242af39aa77f14b4eef500e0b83
Bojana Bogdanov <bojana.bogdanov@lidl.com>
2018-02-26 17:12:35 +0100

    php running

--- run.sh
+++ run.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+# create user defined network to work with host names instead of ip addresses
+if [ $(docker network ls -f 'name=lidl-net' | wc -l) -eq 1 ]; then
+	docker network create lidl-net
+fi
+
+# run php-fpm container
+docker run \
+	--volume $(PWD)/webroot:/var/www/html \
+	--network lidl-net \
+	--name php \
+	--rm \
+	--detach \
+	php:fpm 
+
+# run nginx container
 docker run \
 	--publish 85:80 \
-	--volume $(PWD)/webroot:/usr/share/nginx/html \
+	--volume $(PWD)/lidl.conf:/etc/nginx/conf.d/default.conf  \
+	--volume $(PWD)/webroot:/var/www/html \
+	--network lidl-net \
+	--name nginx \
 	--rm \
 	--detach \
 	nginx 
